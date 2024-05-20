434ca6be9a7b86387897dd1bc48b170b8d95b1e9
Kostes <qaz1234567890838@gmail.com>
2024-05-20 10:31:07 +0300

    метрики

--- flsite.py
+++ flsite.py
@@ -1,7 +1,7 @@
 import pickle
 
 import numpy as np
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
@@ -11,10 +11,39 @@ menu = [{"name": "Главная", "url": "/"},
         {"name": "Логистическая", "url": "p_logistic"},
         {"name": "Дерево", "url": "p_three"}]
 
+metrics_linear_data = [
+    {"name": "MSE", "value": "1.3509243001909827e-29"},
+    {"name": "RMSE", "value": "6.754621500954914e-30"},
+    {"name": "MSPE", "value": "5.403697200763931e-29"},
+    {"name": "MAPE", "value": "8.500000000000002"},
+    {"name": "SMAPE", "value": "1.831867990631502e-15"},
+    {"name": "MASE", "value": "7.327471962526033e-15"},
+    {"name": "MRE", "value": "inf"},
+    {"name": "RMSLE", "value": "2.9624445261614147e-15"},
+    {"name": "R-квадрат", "value": "1.0"},
+    {"name": "Скорректированный R-квадрат", "value": "0.7986743400332134"},
+                ]
+
+metrics_classification_data = [
+    {"name": "Confusion matrix", "type": "logistic", "value": {"Predicted": [73, 15], "Actual": [10, 82]}},
+    {"name": "Confusion matrix", "type": "KNN", "value": {"Predicted": [72, 16], "Actual": [14, 78]}},
+    {"name": "Confusion matrix", "type": "three", "value": {"Predicted": [70, 18], "Actual": [21, 71]}},
+    {"name": "Accuracy", "type": "logistic", "value": 0.8611111111111112},
+    {"name": "Accuracy", "type": "KNN", "value": 0.8333333333333334},
+    {"name": "Accuracy", "type": "three", "value": 0.7833333333333333},
+    {"name": "Precision", "type": "logistic", "value": 0.845360824742268},
+    {"name": "Precision", "type": "KNN", "value": 0.8297872340425532},
+    {"name": "Precision", "type": "three", "value": 0.797752808988764},
+    {"name": "Recall", "type": "logistic", "value": 0.8913043478260869},
+    {"name": "Recall", "type": "KNN", "value": 0.8478260869565217},
+    {"name": "Recall", "type": "three", "value": 0.7717391304347826},
+]
+
 loaded_model_knn = pickle.load(open('model/Iris_pickle_file', 'rb'))
 loaded_model_linear = pickle.load(open('model/linearModel', 'rb'))
 loaded_model_logistic = pickle.load(open('model/logistic_model', 'rb'))
 loaded_model_tree = pickle.load((open('model/Tree_model', 'rb')))
+
 
 @app.route("/")
 def index():
@@ -29,7 +58,8 @@ def f_knn():
         return render_template('lab1.html',
                                title="Метод k -ближайших соседей (KNN)",
                                menu=menu,
-                               class_model='')
+                               class_model='',
+                               metrics=metrics_classification_data)
     if request.method == 'POST':
         x_new = np.array([[float(request.form['list1']),
                            float(request.form['list2']),
@@ -39,13 +69,17 @@ def f_knn():
         return render_template('lab1.html',
                                title="Метод k -ближайших соседей (KNN)",
                                menu=menu,
-                               class_model="Это: " + pred[0])
+                               class_model="Это: " + pred[0],
+                               metrics=metrics_classification_data)
 
 
 @app.route("/p_linear", methods=['POST', 'GET'])
 def f_linear():
     if request.method == 'GET':
-        return render_template('lab2.html', title="Линейная регрессия", menu=menu)
+        return render_template('lab2.html',
+                               title="Линейная регрессия",
+                               menu=menu,
+                               metrics=metrics_linear_data)
     if request.method == 'POST':
         x_new = np.array([[float(request.form['list1']),
                            float(request.form['list2']),
@@ -54,13 +88,17 @@ def f_linear():
         return render_template('lab2.html',
                                title="Линейная регрессия",
                                menu=menu,
-                               class_model=f"Ваш размер обуви: {pred[0]}")
+                               class_model=f"Ваш размер обуви: {pred[0]}",
+                               metrics=metrics_linear_data)
 
 
 @app.route("/p_logistic", methods=['POST', 'GET'])
 def f_logistic():
     if request.method == 'GET':
-        return render_template('lab3.html', title="Логистическая регрессия", menu=menu)
+        return render_template('lab3.html',
+                               title="Логистическая регрессия",
+                               menu=menu,
+                               metrics=metrics_classification_data)
     if request.method == 'POST':
         x_new = np.array([[float(request.form['list1']),
                            float(request.form['list2']),
@@ -68,17 +106,22 @@ def f_logistic():
                            float(request.form['list4']),
                            float(request.form['list5']),
                            float(request.form['list6']),
-                           float(request.form['list7']),]])
+                           float(request.form['list7']), ]])
         pred = loaded_model_logistic.predict(x_new)
         return render_template('lab3.html',
                                title="Логистическая регрессия",
                                menu=menu,
-                               class_model=f"Это: {pred[0]}")
+                               class_model=f"Это: {pred[0]}",
+                               metrics=metrics_classification_data)
+
 
 @app.route("/p_three", methods=['POST', 'GET'])
 def f_three():
     if request.method == 'GET':
-        return render_template('three.html', title="Дерево решений", menu=menu)
+        return render_template('three.html',
+                               title="Дерево решений",
+                               menu=menu,
+                               metrics=metrics_classification_data)
     if request.method == 'POST':
         x_new = np.array([[float(request.form['list1']),
                            float(request.form['list2']),
@@ -91,7 +134,62 @@ def f_three():
         return render_template('three.html',
                                title="Дерево решений",
                                menu=menu,
-                               class_model=f"Это: {pred[0]}")
+                               class_model=f"Это: {pred[0]}",
+                               metrics=metrics_classification_data)
+
+
+@app.route('/api_sort', methods=['GET'])
+def get_sort():
+    request_data = request.get_json()
+    x_new = np.array([[float(request_data['sepal_length']),
+                       float(request_data['sepal_width']),
+                       float(request_data['petal_length']),
+                       float(request_data['petal_width'])]])
+    pred = loaded_model_knn.predict(x_new)
+
+    return jsonify(sort=pred[0])
+
+
+@app.route('/api_linear', methods=['get'])
+def get_linear():
+    request_data = request.get_json()
+    x_new = np.array([[float(request_data['height']),
+                       float(request_data['weight']),
+                       float(request_data['sex'])]])
+    pred = loaded_model_knn.predict(x_new)
+
+    return jsonify(sort=pred[0])
+
+
+@app.route('/api_logistic', methods=['GET'])
+def get_logistic():
+    request_data = request.get_json()
+    x_new = np.array([[float(request_data['area']),
+                       float(request_data['majorAxisLength']),
+                       float(request_data['minorAxisLength']),
+                       float(request_data['eccentricity']),
+                       float(request_data['convexArea']),
+                       float(request_data['extent']),
+                       float(request_data['perimeter']), ]])
+    pred = loaded_model_logistic.predict(x_new)
+
+    return jsonify(sort=pred[0])
+
+
+@app.route('/api_tree', methods=['GET'])
+def get_tree():
+    request_data = request.get_json()
+    x_new = np.array([[float(request_data['size']),
+                       float(request_data['weight']),
+                       float(request_data['sweetness']),
+                       float(request_data['eccentricity']),
+                       float(request_data['convexArea']),
+                       float(request_data['extent']),
+                       float(request_data['perimeter']), ]])
+    pred = loaded_model_logistic.predict(x_new)
+
+    return jsonify(sort=pred[0])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model/y_test.py
+++ model/y_test.py
@@ -1,1 +1,13 @@
+from requests import get
 
+height = input('Введите рост = ')
+weight = input('Введите вес = ')
+sex = input('Введите пол = ')
+# if sex == 'Мужской':
+#     sex = 1
+# else:
+#     sex = 0
+print(get('http://localhost:5000/api_linear',
+          json={'height': height,
+                'weight': weight,
+                'sex': sex}).json())
