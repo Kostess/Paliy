b35917d72fd19312418a8f0ecd36e250a42471ce
Kostes <qaz1234567890838@gmail.com>
2024-11-13 03:15:05 +0300

    модель для одежды готова

--- static/script/index.js
+++ static/script/index.js
@@ -1,8 +1,21 @@
-document.querySelector('#fileInput').addEventListener('change', function() {
+document.querySelector('#fileInput').addEventListener('change', (event) => {
     const label = document.querySelector('label[for="fileInput"]');
-    if (this.files.length > 0) {
-        label.textContent = this.files[0].name;
+    const previewImage = document.querySelector('#previewImage');
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            previewImage.src = reader.result;
+            previewImage.classList.remove('hidden');
+        });
+
+        reader.readAsDataURL(file);
+        label.textContent = file.name;
     } else {
+        previewImage.src = '#';
+        previewImage.classList.add('hidden');
         label.textContent = 'Выберите файл';
     }
 });
