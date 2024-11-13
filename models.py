1473103166443f84249173ca26d370d53f8b1073
Kostess <qaz1234567890838@gmail.com>
2024-11-13 03:57:14 +0300

    ниче не работает

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ loaded_model_tree = pickle.load((open('model/Tree_model', 'rb')))
 new_neuron = SingleNeuron(input_size=3)
 new_neuron.load_weights('model/neuron_weights.txt')
 
+
 model_reg = tf.keras.models.load_model('model/regression_model.h5')
 model_fashion = tf.keras.models.load_model('model/fashion_mnist_model.h5')
 model_food = tf.keras.models.load_model('model/food_1_model.keras')
